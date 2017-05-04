3045ea708f4b35c3fa0e3e52d6d1adf64369b042
lasperilla <l.asperilla@gmail.com>
2017-05-04 14:38:38 -0400

    finished update func, handled case where user inputs invalid item id

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
     database: "Bamazon"
 });
 
-let productIdArr =[];
+let productIdArr = [];
 
 //open connection
 connection.connect(function(err) {
@@ -51,28 +51,38 @@ function orderFunc() {
         message: "Qty:"
     }]).then(function(order) {
         if (productIdArr.indexOf(parseInt(order.id)) !== -1) {
-                connection.query("SELECT stock_quantity, product_name, price FROM products WHERE item_id = " + order.id, function(err, res) {
-                    if (err) throw err;
-                    let subtotal = order.quantity*res[0].price;
-                    let currentInventory = res[0].stock_quantity;
-                    if(order.quantity <= currentInventory){
-                      console.log("We have received your order. Thank you for using Bamazon");
-                      console.log("--------------------");
-                      console.log("Order Summary:");
-                      console.log(res[0].product_name+" Qty:"+order.quantity);
-                      console.log("Subtotal: $"+subtotal.toFixed(2));
-                      connection.end();
-                    } else {
-                      console.log("Sorry, insufficient quantity. Please try again or hit ctrl+c twice to exit.")
-                      orderFunc();
-                    }
-                });
-        } else {
-          console.log("Please enter a valid Item ID or hit ctrl+c twice to exit.");
-          orderFunc();
+            connection.query("SELECT stock_quantity, product_name, price FROM products WHERE item_id = " + order.id, function(err, res) {
+                if (err) throw err;
+                let subtotal = order.quantity * res[0].price;
+                let currentInventory = res[0].stock_quantity;
+                fulfillFunc(order.quantity,currentInventory,order.id); //update database
+                if (order.quantity <= currentInventory) {
+                    console.log("We have received your order. Thank you for using Bamazon");
+                    console.log("--------------------");
+                    console.log("Order Summary:");
+                    console.log(res[0].product_name + " Qty:" + order.quantity);
+                    console.log("Subtotal: $" + subtotal.toFixed(2));
+                    connection.end();
+                } else { //handle case where user inputs qty > bamazon inventory
+                    console.log("Sorry, insufficient quantity. Please try again or hit ctrl+c twice to exit.")
+                    orderFunc();
+                }
+            });
+        } else { //handle case where user inputs invalid item id
+            console.log("Please enter a valid Item ID or hit ctrl+c twice to exit.");
+            orderFunc();
         };
     });
 };
 
 //update database inventory
-function
+function fulfillFunc(orderquant,bamazonInv,id) {
+    let remainingInv = bamazonInv - orderquant;
+    connection.query("UPDATE products SET ? WHERE ?", [{
+        stock_quantity: remainingInv
+    }, {
+        item_id: id
+    }], function(err, res) {
+        if (err) throw err;
+    });
+};
